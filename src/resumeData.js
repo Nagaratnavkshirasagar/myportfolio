32d5dad19aac3a4750b0204932255284f6fb9263
Nagaratna Kshirasagar <nagaratnakshirasagar666666@gmail.com>
2020-02-14 18:04:31 +0530

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "NAGARATNA V KSHIRASAGAR",
+    "role": "STUDENT",
+    "linkedinId":"Nagaratna Kshirasagar",
+    "skypeid": "no",
+    "roleDescription": "I like to code in java and like to learn about new technologies, watch webseries and read novels in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/nagaratna-kshirasagar-aaa7581a0/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Nagaratnavkshirasagar/myportfolio",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"gmail",
+          "url":"nagaratnakshirasagar666666@gmail.com",
+          "className":"fa fa-gmailid"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at KLE'S BCA,HUBBALLI and pursuing my BCA from here. I am a self taught java, currently diving deeper into Android development. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://github.com/Nagaratnavkshirasagar/myportfolio",
+    "education":[
+      {
+        "UniversityName":"The KLE'S BCA,HUBBALLI",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"may",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Delloitte",
+        "specialization":"html,java",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"wipro",
+        "specialization":"html,java",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"debugging"
+      },
+      {
+        "skillname":"java"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
